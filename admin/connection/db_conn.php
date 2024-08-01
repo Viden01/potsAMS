9e0e8541926e63aa7cd878a730964d1f0d98d85e
Eden Villabrille <edenvillabrille8@gmail.com>
2024-08-01 13:56:33 +0100

    commit

--- admin/connection/db_conn.php
+++ admin/connection/db_conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'attendance_tracking');
+	$conn = new mysqli('127.0.0.1', 'u510162695_potsesl', '1Potsesl', 'u510162695_potsesl');
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
