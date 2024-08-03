77274158d4202b9282dee2e3e4873aba634067d1
Eden Villabrille <edenvillabrille8@gmail.com>
2024-08-03 20:14:47 +0100

    commit

--- admin/private/header/head.php
+++ admin/private/header/head.php
@@ -89,10 +89,7 @@ if (!isset($_SESSION["email_address"])) {
                     </a>
                     <!-- dropdown user-->
                     <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#"><i class="fa fa-user fa-fw"></i>User Profile</a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i>Settings</a>
-                        </li>
+                        
                         <li class="divider"></li>
                         <li><a href="Logout.php"><i class="fa fa-sign-out fa-fw"></i>Logout</a>
                         </li>
--- admin/private/header/sidebar_menu.php
+++ admin/private/header/sidebar_menu.php
@@ -1,3 +1,7 @@
+<?php
+$name = "Julius Maru";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
