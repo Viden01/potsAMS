5c6d9eb6e968deb8567fe14e527f105c290d45ad
Eden Villabrille <edenvillabrille8@gmail.com>
2024-08-03 20:41:22 +0100

    commit

--- index.php
+++ index.php
@@ -1,45 +1,49 @@
-<?php include('header/head.php');?>
+<?php include('header/head.php'); ?>
 <body>
-   <div class="container mb-2">
-      <!-- Footer -->
-      <footer class="page-footer mt-2" style="margin-bottom: 4px;background-color: #17a2b8;;font-size: 150%;">
-         <div class="footer-copyright text-center py-2">
-            <p id="date"></p>
-            <font color="#ff9999">
-               <p id="time" style="font-weight: bold;font-size: 250%;font-family:'digital-clock-font';"></p>
-            </font>
-         </div>
-      </footer>
-      <!-- Footer -->
-      <div class="row">
-         <div class="col-sm">
-            <!-- Card -->
-            <div class="card" style="border:1px solid #d4edda;height: 100%;width: 100%;background-color: #d4edda!important;">
-               <div id="alert-message-success" class="alert alert-success alert-dismissible mt20 text-center" style="display:none;">
-                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                  <span class="result"><i class="fas fa-tachometer-alt"></i> <span class="message"></span></span>
-               </div>
-               <div id="alert-message-danger" class="alert alert-danger alert-dismissible mt20 text-center" style="display:none;">
-                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                  <span class="result"><i class="icon fa fa-warning"></i> <span class="message"></span></span>
-               </div>
+    <div class="container mb-2">
+        <!-- Footer -->
+        <footer class="page-footer mt-2" style="margin-bottom: 4px;background-color: #17a2b8;font-size: 150%;">
+            <div class="footer-copyright text-center py-2">
+                <p id="date"></p>
+                <font color="#ff9999">
+                    <p id="time" style="font-weight: bold;font-size: 250%;font-family:'digital-clock-font';"></p>
+                </font>
             </div>
-            <!-- Card -->
-         </div>
-         <div class="col-sm">
-            <!-- Form -->
-            <form class="text-center border border-light p-5" id="punch">
-               <p class="h4 mb-3">ATTENDANCE TRACKER</p>
-               <input type="text" id="employee_id" name="employee_id" class="form-control mb-4" placeholder="Employee ID" autocomplete="off" required="">
-               <select class="browser-default custom-select mb-4" name="status" autofocus="off" required="">
-                  <option value="in" selected>Time In</option>
-                  <option value="out">Time Out</option>
-               </select>
-               <button type="submit" class="btn btn-info btn-block">Punch</button>
-            </form>
-            <!-- Form -->
-         </div>
-      </div>
-      <?php include('footer/footer.php');?>
+        </footer>
+        <!-- Footer -->
+        <div class="row">
+            <div class="col-sm">
+                <!-- Card -->
+                <div class="card" style="border:1px solid #d4edda;height: 100%;width: 100%;background-color: #d4edda!important;">
+                    <div id="alert-message-success" class="alert alert-success alert-dismissible mt20 text-center" style="display:none;">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        <span class="result"><i class="fas fa-tachometer-alt"></i> <span class="message"></span></span>
+                    </div>
+                    <div id="alert-message-danger" class="alert alert-danger alert-dismissible mt20 text-center" style="display:none;">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        <span class="result"><i class="icon fa fa-warning"></i> <span class="message"></span></span>
+                    </div>
+                </div>
+                <!-- Card -->
+            </div>
+            <div class="col-sm">
+                <!-- Form -->
+                <form class="text-center border border-light p-5" id="punch" action="attendance_process.php" method="POST">
+                    <p class="h4 mb-3">ATTENDANCE TRACKER</p>
+                    <input type="text" id="employee_id" name="employee_id" class="form-control mb-4" placeholder="Employee ID" autocomplete="off" required="">
+                    <select class="browser-default custom-select mb-4" name="status" required="">
+                        <option value="in" selected>Time In</option>
+                        <option value="out">Time Out</option>
+                    </select>
+                    <input type="time" id="time_in" name="time_in" class="form-control mb-4" placeholder="Time In" required="">
+                    <input type="time" id="time_out" name="time_out" class="form-control mb-4" placeholder="Time Out">
+                    <input type="date" id="date_attendance" name="date_attendance" class="form-control mb-4" placeholder="Date" required="">
+                    <button type="submit" class="btn btn-info btn-block">Punch</button>
+                </form>
+                <!-- Form -->
+            </div>
+        </div>
+        <?php include('footer/footer.php'); ?>
+    </div>
 </body>
 </html>
