e3ddd46faebbe82669ef9a396bd4342e586c461d
Eden Villabrille <edenvillabrille8@gmail.com>
2024-08-07 08:09:23 +0100

    commit

--- admin/private/payroll.php
+++ admin/private/payroll.php
@@ -20,7 +20,7 @@ if (isset($_GET['range'])) {
 <div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Dashboard</h1>
+          
         </div>
     </div>
 
