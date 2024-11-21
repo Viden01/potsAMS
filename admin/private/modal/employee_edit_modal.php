72761a2135857a84750c931c5cdd083f29da0589
Viden01 <edenvillabrille8@gmail.com>
2024-11-22 02:11:15 +0800

    commit

--- admin/private/modal/employee_edit_modal.php
+++ admin/private/modal/employee_edit_modal.php
@@ -1,4 +1,4 @@
-<!-- Edit -->
+<!-- Add -->
 <div class="modal fade" id="edit">
    <div class="modal-dialog">
       <div class="modal-content">
@@ -26,17 +26,9 @@
                   </div>
                </div>
                <div class="form-row">
-                  <div class="form-group col-md-4">
-                     <label for="Barangay">Barangay</label>
-                     <input type="text" class="form-control" id="edit_barangay" name="barangay" autocomplete="off">
-                  </div>
-                  <div class="form-group col-md-4">
-                     <label for="Municipality">Municipality</label>
-                     <input type="text" class="form-control" id="edit_municipality" name="municipality" autocomplete="off">
-                  </div>
-                  <div class="form-group col-md-4">
-                     <label for="City">City</label>
-                     <input type="text" class="form-control" id="edit_city" name="city" autocomplete="off">
+                  <div class="form-group col-md-12">
+                     <label for="CompleteAddress">Complete Address</label>
+                     <input type="text" class="form-control" id="edit_address" alt="complete_address" name="complete_address" autocomplete="off">
                   </div>
                </div>
                <div class="form-row">
@@ -46,7 +38,7 @@
                   </div>
                   <div class="form-group col-md-6">
                      <label for="MobileNumber">Mobile Number</label>
-                     <input type="text" class="form-control" id="edit_mobilenumber" alt="Mobile_number" name="Mobile_number" maxlength="11" autocomplete="off">
+                     <input type="text" class="form-control" id="edit_mobilenumber" alt="Mobile_number" name="Mobile_number"  maxlength="11" maxlength="11" autocomplete="off">
                   </div>
                </div>
                <div class="form-row">
@@ -104,6 +96,10 @@
                   <label for="MobileNumber">Profile</label>
                   <input type="file" id="edit_profilepic" class="form-control" alt="profile_pic" name ="profile_pic"><br>
                </div>
+               <div class="form-group col-md-0">
+                  <!--                      <label for="MobileNumber">Image profile</label><br>
+                     <img src="" class="img img-thumbnail" style="max-height: 150px!important" id="edit_profilepic" > -->
+               </div>
             </div>
             <div class="modal-footer">
                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
@@ -113,4 +109,63 @@
       </div>
    </div>
 </div>
-<!--end Edit Modal -->
+<!--end Add Modal -->
+<script type="text/javascript">
+   $(document).ready(function() {
+   
+    document.getElementById("editemp").addEventListener("click", (e) =>{
+       e.preventDefault();
+   
+      const first_name = document.querySelector('input[id=edit_firstname]').value;
+      const middle_name = document.querySelector('input[id=edit_middlename]').value;
+      const last_name = document.querySelector('input[id=edit_lastname]').value;
+      const complete_address = document.querySelector('input[id=edit_address]').value;
+      const birth_date = document.querySelector('input[id=edit_birthdate]').value;
+      const Mobile_number = document.querySelector('input[id=edit_mobilenumber]').value;
+      const gender = ($('.gender option:selected').val());
+      const position_id = ($('.position_id option:selected').val());
+      const marital_status = ($('.marital_status option:selected').val());
+      const schedule_id = ($('.schedule_id option:selected').val());
+      const emp_id = document.querySelector('input[id=emp_id]').value;
+   
+   
+         var delay = 100;
+               var data = new FormData(this.form);
+               data.append('first_name', first_name);
+               data.append('middle_name', middle_name);
+               data.append('last_name', last_name);
+               data.append('complete_address', complete_address);
+               data.append('birth_date', birth_date);
+               data.append('Mobile_number', Mobile_number);
+               data.append('gender', gender);
+               data.append('position_id', position_id);
+               data.append('marital_status', marital_status);
+               data.append('schedule_id', schedule_id);
+               data.append('profile_pic', $('#edit_profilepic')[0].files[0]);
+               data.append('emp_id', emp_id);
+   
+            $.ajax({
+                url: 'processing/Edit_employee_process.php',
+                type: "POST",
+                data: data,
+                processData: false,
+                contentType: false,
+   
+                async: false,
+                cache: false,
+   
+                success: function(data) {
+                    setTimeout(function() {
+                        $('#msg').html(data);
+                    }, delay);
+                    setTimeout(location.reload.bind(location), 200);
+   
+                },
+                error: function(data) {
+                    console.log("Failed");
+                }
+            });
+   
+        });
+    });
+</script>
