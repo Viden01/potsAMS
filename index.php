71d721d12d2e9b993cc440e5647610b82e530f9c
Viden01 <edenvillabrille8@gmail.com>
2024-11-23 02:12:42 +0800

    commit

--- index.php
+++ index.php
@@ -35,79 +35,18 @@
          </div>
          <div class="col-sm">
             <!-- Form -->
-            <form class="text-center border border-light p-5" id="punch" method="POST" enctype="multipart/form-data">
+            <form class="text-center border border-light p-5" id="punch">
                <p class="h4 mb-3">ATTENDANCE TRACKER</p>
-               
-               <!-- Employee ID -->
                <input type="text" id="employee_id" name="employee_id" class="form-control mb-4" placeholder="Employee ID" autocomplete="off" required="">
-
-               <!-- Status: Time In or Time Out -->
                <select class="browser-default custom-select mb-4" name="status" autofocus="off" required="">
                   <option value="in" selected>Time In</option>
                   <option value="out">Time Out</option>
                </select>
-
-               <!-- Geocam: Take Selfie and Capture Location -->
-               <div class="mb-4">
-                  <video id="video" width="300" height="200" autoplay></video><br>
-                  <button type="button" id="capture" class="btn btn-primary btn-block">Capture Selfie</button>
-                  <canvas id="canvas" style="display:none;"></canvas>
-               </div>
-               
-               <!-- Hidden Inputs for Selfie and Location -->
-               <input type="hidden" id="location" name="location" value="">
-               <input type="hidden" id="selfie" name="selfie" value="">
-
-               <button type="submit" class="btn btn-info btn-block" id="submitButton" disabled>Punch</button>
+               <button type="submit" class="btn btn-info btn-block">Punch</button>
             </form>
             <!-- Form -->
          </div>
       </div>
-   </div>
-
-   <script>
-      // Get video stream from camera
-      const video = document.getElementById('video');
-      const canvas = document.getElementById('canvas');
-      const captureButton = document.getElementById('capture');
-      const submitButton = document.getElementById('submitButton');
-      const locationInput = document.getElementById('location');
-      const selfieInput = document.getElementById('selfie');
-
-      // Get user media (video) for webcam
-      navigator.mediaDevices.getUserMedia({ video: true })
-         .then((stream) => {
-            video.srcObject = stream;
-         })
-         .catch((err) => {
-            console.log("Error accessing the camera: " + err);
-         });
-
-      // Capture selfie
-      captureButton.addEventListener('click', () => {
-         // Draw current frame on canvas
-         const context = canvas.getContext('2d');
-         context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-         // Convert canvas image to base64 string
-         const selfieData = canvas.toDataURL('image/jpeg');
-         selfieInput.value = selfieData;
-
-         // Get geolocation
-         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-               const latitude = position.coords.latitude;
-               const longitude = position.coords.longitude;
-               
-               locationInput.value = latitude + ',' + longitude;
-
-               // Enable the submit button after selfie and location are captured
-               submitButton.disabled = false;
-            });
-         } else {
-            alert("Geolocation is not supported by this browser.");
-         }
-      });
-   </script>
+      <?php include('footer/footer.php'); ?>
 </body>
 </html>
