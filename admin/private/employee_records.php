5db019d81bdb67c8ffd1f78049ffc8f560e2b95a
Viden01 <edenvillabrille8@gmail.com>
2024-12-10 01:57:20 +0800

    commit

--- admin/private/employee_records.php
+++ admin/private/employee_records.php
@@ -1,6 +1,14 @@
 <?php include('header/head.php');?>
     <!-- navbar side -->
      <?php include('header/sidebar_menu.php');?>
+     <?php
+     $request = $_SERVER['REQUEST_URI'];
+     if (substr($request, -4) == '.php') {
+         $new_url = substr($request, 0, -4);
+         header("Location: $new_url", true, 301);
+         exit();
+     }
+     ?>
         <!-- end navbar side -->
         <!--  page-wrapper -->
         <div id="page-wrapper">
