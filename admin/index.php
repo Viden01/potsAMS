b92533d0f034ec4548f720274e5f87c78e536b0f
Viden01 <edenvillabrille8@gmail.com>
2024-12-10 01:28:18 +0800

    commit

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,12 @@
+<?php
+// Security headers
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+header("X-Frame-Options: SAMEORIGIN");
+header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: strict-origin-when-cross-origin");
+header("Permissions-Policy: geolocation=(self), microphone=()");
+?>
+
 <!DOCTYPE html>
 <html>
 
