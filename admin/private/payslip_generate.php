ed7098dd05eb7cd95e3067e23026c6371b753955
Viden01 <edenvillabrille8@gmail.com>
2024-12-11 02:50:58 +0800

    commit

--- admin/private/payslip_generate.php
+++ admin/private/payslip_generate.php
@@ -1,12 +1,4 @@
 <?php
-$request = $_SERVER['REQUEST_URI'];
-if (substr($request, -4) == '.php') {
-    $new_url = substr($request, 0, -4);
-    header("Location: $new_url", true, 301);
-    exit();
-}
-
-
 include '../connection/db_conn.php';
 
 // Initialize output buffering
