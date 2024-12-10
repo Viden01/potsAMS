053cabf43bf9e8cb1df7c3134c2ee309ef2f40b3
Viden01 <edenvillabrille8@gmail.com>
2024-12-11 03:00:18 +0800

    commit

--- admin/private/payroll_generate.php
+++ admin/private/payroll_generate.php
@@ -1,4 +1,12 @@
 <?php
+$request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
+
+
 include '../connection/db_conn.php';
 
 function generateRow($from, $to, $conn, $deduction) {
