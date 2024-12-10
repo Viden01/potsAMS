d4abb45c131be430b3b93254a752aea0b8bce014
Viden01 <edenvillabrille8@gmail.com>
2024-12-11 04:03:10 +0800

    commit

--- admin/private/header/head.php
+++ admin/private/header/head.php
@@ -6,39 +6,35 @@ if (!isset($_SESSION["email_address"])) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>POTS - ESL</title>
-    <!-- Core CSS -->
-    <link href="assets/plugins/bootstrap/bootstrap.css" rel="stylesheet">
-    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet">
-    <link href="assets/plugins/pace/pace-theme-big-counter.css" rel="stylesheet">
+    <!-- Core CSS - Include with every page -->
+    <link href="assets/plugins/bootstrap/bootstrap.css" rel="stylesheet" />
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/plugins/pace/pace-theme-big-counter.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="assets/css/style.css" rel="stylesheet">
-    <link href="assets/css/main-style.css" rel="stylesheet">
-    <!-- Additional CSS -->
-    <link href="assets/plugins/morris/morris-0.4.3.min.css" rel="stylesheet">
-    <link href="assets/plugins/dataTables/dataTables.bootstrap.css" rel="stylesheet">
-    <style>
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <link href="assets/css/main-style.css" rel="stylesheet" />
+    <!-- Page-Level CSS -->
+    <link href="assets/plugins/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <link href="assets/plugins/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
+    <style type="text/css">
         .navbar-fixed-top {
             background-color: #007bff;
             color: #fff;
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
         }
-        .navbar-brand {
+        .navbar-brand h2 {
             color: #fff;
             font-size: 1.5rem;
+            margin: 0;
         }
-        .navbar-brand:hover {
-            color: #e9ecef;
-        }
-        .navbar-toggle {
-            border-color: #fff;
-        }
-        .navbar-toggle .icon-bar {
-            background-color: #fff;
+        .navbar-top-links .dropdown-toggle {
+            color: #fff;
+            font-size: 1rem;
         }
         .dropdown-menu {
             background-color: #f8f9fa;
@@ -51,55 +47,62 @@ if (!isset($_SESSION["email_address"])) {
             background-color: #007bff;
             color: #fff;
         }
+        .fa-user {
+            margin-right: 8px;
+        }
     </style>
 </head>
 <body>
+    <!--  wrapper -->
     <div id="wrapper">
-        <!-- Navbar Top -->
+        <!-- navbar top -->
         <nav class="navbar navbar-default navbar-fixed-top" role="navigation" id="navbar">
-            <div class="container-fluid">
-                <!-- Navbar Header -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.html">
-                        POTS - ESL
+            <!-- navbar-header -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">
+                    <h2>POTS - ESL</h2>
+                </a>
+            </div>
+            <!-- end navbar-header -->
+            <!-- navbar-top-links -->
+            <ul class="nav navbar-top-links navbar-right">
+                <?php 
+                include '../connection/db_conn.php';
+                if (isset($_SESSION['email_address'])) {
+                    $email = $conn->real_escape_string($_SESSION['email_address']);
+                    $result = $conn->query("SELECT * FROM login_admin WHERE email_address = '$email'") or die($conn->error);
+
+                    if ($result->num_rows > 0) {
+                        $row = $result->fetch_array();
+                        $userName = htmlentities($row['name']);
+                    } else {
+                        $userName = "Unknown";
+                    }
+                } else {
+                    $userName = "Guest";
+                }
+                ?>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user"></i> Welcome, <?php echo ucwords($userName); ?>
                     </a>
-                </div>
-                <!-- Navbar Links -->
-                <div class="collapse navbar-collapse" id="navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <?php
-                        include '../connection/db_conn.php';
-                        $userName = "Guest";
-                        if (isset($_SESSION['email_address'])) {
-                            $email = $conn->real_escape_string($_SESSION['email_address']);
-                            $result = $conn->query("SELECT * FROM login_admin WHERE email_address = '$email'");
-                            if ($result->num_rows > 0) {
-                                $row = $result->fetch_assoc();
-                                $userName = htmlentities($row['name']);
-                            }
-                        }
-                        ?>
-                        <li class="dropdown">
-                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                                <i class="fa fa-user"></i> Welcome, <?php echo ucwords($userName); ?> <b class="caret"></b>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a href="Logout.php"><i class="fa fa-sign-out"></i> Logout</a></li>
-                            </ul>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li>
+                            <a href="Logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
                         </li>
                     </ul>
-                </div>
-            </div>
+                </li>
+            </ul>
+            <!-- end navbar-top-links -->
         </nav>
+        <!-- end navbar top -->
     </div>
-    <!-- Include JS -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js"></script>
+    <!-- end wrapper -->
 </body>
 </html>
