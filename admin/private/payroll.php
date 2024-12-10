e77366f3527784d5d190affd49b624cf5b2954c1
Viden01 <edenvillabrille8@gmail.com>
2024-12-11 02:49:12 +0800

    commit

--- admin/private/payroll.php
+++ admin/private/payroll.php
@@ -1,4 +1,12 @@
-<?php include('header/head.php');?>
+<?php
+$request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
+
+include('header/head.php');?>
 <?php include('header/sidebar_menu.php');?>
 <?php
 include('header/timezone.php');
