c98de0c43041c4a4656aa643588c927ca95b095a
Viden01 <edenvillabrille8@gmail.com>
2024-12-11 08:37:37 +0800

    commit

--- admin/private/header/head.php
+++ admin/private/header/head.php
@@ -2,7 +2,6 @@
 session_start();
 if (!isset($_SESSION["email_address"])) {
     header("location:../index.php");
-    exit();
 }
 ?>
 <!DOCTYPE html>
@@ -10,7 +9,7 @@ if (!isset($_SESSION["email_address"])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>POTS - ESL</title>
+    <title>POTS - ESL </title>
     <!-- Core CSS - Include with every page -->
     <link href="assets/plugins/bootstrap/bootstrap.css" rel="stylesheet" />
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
@@ -18,37 +17,27 @@ if (!isset($_SESSION["email_address"])) {
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet" />
     <link href="assets/css/main-style.css" rel="stylesheet" />
+    <link href="css/timepicki.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <!-- Page-Level CSS -->
     <link href="assets/plugins/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <!-- Page-Level CSS -->
     <link href="assets/plugins/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
     <style type="text/css">
+        .zoomin img {
+            height: 35px;
+            width: 35px;
+            -webkit-transition: all 2s ease;
+            -moz-transition: all 2s ease;
+            -ms-transition: all 2s ease;
+            transition: all 2s ease;
+        }
+        .zoomin img:hover {
+            width: 100px;
+            height: 100px;
+        }
         .navbar-fixed-top {
-            background-color: #007bff;
-            color: #fff;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-        }
-        .navbar-brand h2 {
-            color: #fff;
-            font-size: 1.5rem;
-            margin: 0;
-        }
-        .navbar-top-links .dropdown-toggle {
-            color: #fff;
-            font-size: 1rem;
-        }
-        .dropdown-menu {
-            background-color: #f8f9fa;
-            border-radius: 5px;
-        }
-        .dropdown-menu a {
-            color: #333;
-        }
-        .dropdown-menu a:hover {
-            background-color: #007bff;
-            color: #fff;
-        }
-        .fa-user {
-            margin-right: 8px;
+            background-color: skyblue;
         }
     </style>
 </head>
@@ -66,38 +55,48 @@ if (!isset($_SESSION["email_address"])) {
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="index.html">
-                    <h2>POTS - ESL</h2>
+                   <h2 style="color: #fff">POTS - ESL</h2>
                 </a>
             </div>
             <!-- end navbar-header -->
             <!-- navbar-top-links -->
             <ul class="nav navbar-top-links navbar-right">
+                <!-- main dropdown -->
                 <?php 
                 include '../connection/db_conn.php';
+
                 if (isset($_SESSION['email_address'])) {
                     $email = $conn->real_escape_string($_SESSION['email_address']);
-                    $result = $conn->query("SELECT * FROM login_admin WHERE email_address = '$email'") or die($conn->error);
+                    $r = $conn->query("SELECT * FROM login_admin WHERE email_address = '$email'") or die ($conn->error);
 
-                    if ($result->num_rows > 0) {
-                        $row = $result->fetch_array();
-                        $userName = htmlentities($row['name']);
+                    if ($r->num_rows > 0) {
+                        $row = $r->fetch_array();
+                        $id = htmlentities($row['email_address']);
+                        $ids = htmlentities($row['id']);
+                        $name = htmlentities($row['name']);
                     } else {
-                        $userName = "Unknown";
+                        // Handle case where no rows are returned
+                        $id = $ids = $name = "Unknown";
                     }
                 } else {
-                    $userName = "Guest";
+                    // Handle case where session variable is not set
+                    $id = $ids = $name = "Unknown";
                 }
                 ?>
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user"></i> Welcome, <?php echo ucwords($userName); ?>
+                       Welcome!,</font> <?php echo ucwords(htmlentities($id)); ?> <i class="fa fa-user fa-3x"></i>
                     </a>
+                    <!-- dropdown user-->
                     <ul class="dropdown-menu dropdown-user">
-                        <li>
-                            <a href="Logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        
+                        <li class="divider"></li>
+                        <li><a href="Logout.php"><i class="fa fa-sign-out fa-fw"></i>Logout</a>
                         </li>
                     </ul>
+                    <!-- end dropdown-user -->
                 </li>
+                <!-- end main dropdown -->
             </ul>
             <!-- end navbar-top-links -->
         </nav>
