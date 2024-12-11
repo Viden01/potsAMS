33b0c87b2ec3053b3bdd7b57005138e771483f5c
Viden01 <edenvillabrille8@gmail.com>
2024-12-11 08:34:38 +0800

    commit

--- admin/private/position.php
+++ admin/private/position.php
@@ -1,11 +1,4 @@
 <?php
-$request = $_SERVER['REQUEST_URI'];
-if (substr($request, -4) == '.php') {
-    $new_url = substr($request, 0, -4);
-    header("Location: $new_url", true, 301);
-    exit();
-}
-
 include('header/head.php');?>
 <?php include('header/sidebar_menu.php');?>
 
