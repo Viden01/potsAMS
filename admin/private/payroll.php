48e7d25c521dba7086bff26d8e38788daf336f95
Viden01 <edenvillabrille8@gmail.com>
2024-12-13 04:30:50 +0800

    commit

--- admin/private/payroll.php
+++ admin/private/payroll.php
@@ -28,7 +28,6 @@ if (isset($_GET['range'])) {
 <div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
-          
         </div>
     </div>
 
@@ -48,7 +47,6 @@ if (isset($_GET['range'])) {
         <?php else: ?>
             <div class="col-lg-12">
                 <div class="alert alert-info">
-                    
                 </div>
             </div>
             <div class="form-row">
@@ -88,13 +86,11 @@ if (isset($_GET['range'])) {
                         <?php
                         include '../connection/db_conn.php';
 
-                        // Fetch total deductions
                         $sql = "SELECT SUM(amount) as total_amount FROM employee_deductions";
                         $query = $conn->query($sql);
                         $drow = $query->fetch_assoc();
                         $deduction = isset($drow['total_amount']) ? $drow['total_amount'] : 0;
 
-                        // Fetch employee attendance and calculate totals
                         $sql = "SELECT employee_records.*, employee_position.*, 
                                 SUM(TIME_TO_SEC(TIMEDIFF(time_out, time_in))) AS total_seconds 
                                 FROM employee_attendance 
@@ -108,24 +104,20 @@ if (isset($_GET['range'])) {
                         while ($row = $query->fetch_assoc()) {
                             $empid = $row['emp_id'];
 
-                            // Fetch cash advance
                             $casql = "SELECT SUM(amount) AS cashamount FROM employee_cashadvance WHERE employee_id='$empid' AND date_created BETWEEN '$from' AND '$to'";
                             $caquery = $conn->query($casql);
                             $carow = $caquery->fetch_assoc();
                             $cashadvance = isset($carow['cashamount']) ? $carow['cashamount'] : 0;
 
-                            // Convert total seconds to HH:MM:SS format and decimal hours
                             $total_seconds = $row['total_seconds'];
                             $total_hours = floor($total_seconds / 3600);
                             $total_minutes = floor(($total_seconds % 3600) / 60);
-                            $total_seconds_display = $total_seconds % 60; // Ensure correct seconds display
+                            $total_seconds_display = $total_seconds % 60;
                             $decimal_hours = $total_hours + ($total_minutes / 60) + ($total_seconds_display / 3600);
 
-                            // Format total hours worked as HH:MM:SS
                             $formatted_total_hr = sprintf('%02d:%02d:%02d', $total_hours, $total_minutes, $total_seconds_display);
 
-                            // Calculate gross, deductions, and net pay
-                            $gross = $row['rate_per_hour'] * $decimal_hours; // Using decimal hours
+                            $gross = $row['rate_per_hour'] * $decimal_hours;
                             $total_deduction = $deduction + $cashadvance;
                             $net = $gross - $total_deduction;
 
@@ -169,11 +161,6 @@ if (isset($_GET['range'])) {
             delID(id);
         });
 
-        $("#reservation").on('change', function(){
-            var range = encodeURI($(this).val());
-            window.location = 'payroll.php?range='+range;
-        });
-
         $('#payroll').click(function(e){
             e.preventDefault();
             $('#payForm').attr('action', 'payroll_generate.php');
@@ -184,6 +171,19 @@ if (isset($_GET['range'])) {
             e.preventDefault();
             $('#payForm').attr('action', 'payslip_generate.php');
             $('#payForm').submit();
+        });
+
+        $('#reservation').daterangepicker({
+            locale: {
+                format: 'MM/DD/YYYY'
+            },
+            startDate: '<?php echo $range_from; ?>',
+            endDate: '<?php echo $range_to; ?>',
+        });
+
+        $("#reservation").on('apply.daterangepicker', function(ev, picker) {
+            var range = encodeURI($(this).val());
+            window.location = 'payroll.php?range=' + range;
         });
     });
 
@@ -216,6 +216,11 @@ if (isset($_GET['range'])) {
     }
     </script>
 
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+
     <script src="assets/plugins/jquery-1.10.2.js"></script>
     <script src="assets/plugins/bootstrap/bootstrap.min.js"></script>
     <script src="assets/plugins/metisMenu/jquery.metisMenu.js"></script>
@@ -223,7 +228,6 @@ if (isset($_GET['range'])) {
     <script src="assets/scripts/siminta.js"></script>
     <script src="assets/plugins/dataTables/jquery.dataTables.js"></script>
     <script src="assets/plugins/dataTables/dataTables.bootstrap.js"></script>
-
     <script>
         $(document).ready(function () {
             $('#dataTables-example').dataTable();
