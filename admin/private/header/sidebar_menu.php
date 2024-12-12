7845cee0b66513cfc5c2aa412af89689741c4517
Viden01 <edenvillabrille8@gmail.com>
2024-12-13 01:40:21 +0800

    commit

--- admin/private/header/sidebar_menu.php
+++ admin/private/header/sidebar_menu.php
@@ -10,7 +10,7 @@ $name = "Julius Maru";
     <title>Sidebar Menu</title>
     <style>
         .sidebar-collapse ul.nav li a {
-            background-color: rgb(48, 83, 83);
+            background-color: rgb(48 83 83);
             color: white;
         }
 
@@ -23,45 +23,74 @@ $name = "Julius Maru";
 </head>
 <body>
     <nav class="navbar-default navbar-static-side" role="navigation">
+        <!-- sidebar-collapse -->
         <div class="sidebar-collapse">
+            <!-- side-menu -->
             <ul class="nav" id="side-menu">
                 <li>
+                    <!-- user image section-->
                     <div class="user-section">
                         <div class="user-section-inner">
-                            <img src="assets/img/user.jpg" alt="User Image">
+                            <img src="assets/img/user.jpg" alt="">
                         </div>
                         <div class="user-info">
-                            <div><strong><?php echo ucwords(htmlentities($name)); ?></strong></div>
+                            <div> <strong><?php echo ucwords(htmlentities($name)); ?></strong></div>
                             <div class="user-text-online">
                                 <span class="user-circle-online btn btn-success btn-circle"></span>&nbsp;Online
                             </div>
                         </div>
                     </div>
+                    <!--end user image section-->
                 </li>
 
-                <li>
-                    <a href="dashboard.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                <li class="selected">
+                    <a href="dashboard.php"><i class="fa fa-dashboard fa-fw"></i>Dashboard</a>
                 </li>
                 <li>
-                    <a href="attendance.php"><i class="fa fa-history fa-fw"></i> Attendance</a>
+                    <a href="#"><i class="fa fa-history fa-fw"></i> Attendance<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="attendance.php">&rarr; Attendance records</a>
+                        </li>
+                    </ul>
+                    <!-- second-level-items -->
                 </li>
                 <li>
-                    <a href="employee_records.php"><i class="fa fa-users fa-fw"></i> Employee Records</a>
+                    <a href="#"><i class="fa fa-users fa-fw"></i> Employee<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="employee_records.php">&rarr; Employee records</a>
+                        </li>
+                       
+                        <li>
+                            <a href="schedule.php">&rarr; Schedule</a>
+                        </li>
+                        <li>
+                            <a href="cashadvance.php">&rarr; Cash Advance</a>
+                        </li>
+                        <li>
+                            <a href="position.php">&rarr; Position</a>
+                        </li>
+                    </ul>
+                    <!-- second-level-items -->
                 </li>
-                <li>
-                    <a href="schedule.php"><i class="fa fa-calendar fa-fw"></i> Schedule</a>
-                </li>
+
                 <li>
                     <a href="deduction.php"><i class="fa fa-money fa-fw"></i> Deduction</a>
                 </li>
                 <li>
                     <a href="payroll.php"><i class="fa fa-file fa-fw"></i> Payroll</a>
                 </li>
+                <!--<li>
+                    <a href="user_monitor.php"><i class="fa fa-eye fa-fw"></i> Admin logged</a>
+                </li> -->
                 <li>
                     <a href="payroll.php?show=generate"><i class="fa fa-file fa-fw"></i> Reports</a>
                 </li>
             </ul>
+            <!-- end side-menu -->
         </div>
+        <!-- end sidebar-collapse -->
     </nav>
 </body>
 </html>
