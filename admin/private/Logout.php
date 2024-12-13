9edb9ed12a2f60e761a30d738174ae3edfd6b666
Viden01 <edenvillabrille8@gmail.com>
2024-12-14 03:23:17 +0800

    commit

--- admin/private/Logout.php
+++ admin/private/Logout.php
@@ -1,38 +1,25 @@
+
 <?php
+
 include '../connection/db_conn.php';
+// this is logout page when user click button logout in system page
 
 session_start();
-date_default_timezone_set("Asia/Manila");
-$time = date("M-d-Y h:i A", strtotime("+0 HOURS"));
+  date_default_timezone_set("Asia/Manila");
+  $time = date("M-d-Y h:i A",strtotime("+0 HOURS"));
 
-if (isset($_SESSION['email_address'])) {
-    $email = $_SESSION['email_address'];
+ $email = $_SESSION['email_address'];
+  
 
-    $stmt = $conn->prepare("UPDATE history_log SET `logout_time` = ? WHERE `id` = ?");
-    $stmt->bind_param("ss", $time, $email);
+$conn->query("UPDATE history_log SET `logout_time` = '$time'  WHERE `id` = '$email'");
 
-    if ($stmt->execute()) {
-        $_SESSION = [];
-        session_unset();
-        session_destroy();
+$_SESSION = NULL;
+$_SESSION = [];
+session_unset();
+session_destroy();
 
-        echo "<script type='text/javascript'>
-            alert('LogOut Successfully!');
-            document.location='../index.php';
-        </script>";
-    } else {
-        echo "<script type='text/javascript'>
-            alert('Error updating logout time. Please try again.');
-            document.location='../index.php';
-        </script>";
-    }
-    $stmt->close();
-} else {
-    echo "<script type='text/javascript'>
-        alert('Session not found. Please log in again.');
-        document.location='../index.php';
-    </script>";
-}
+echo "<script type='text/javascript'>alert('LogOut Successfully!');
+				  document.location='../index.php'</script>";
 
-$conn->close();
 ?>
+
