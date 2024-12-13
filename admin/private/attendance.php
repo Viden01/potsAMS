92c1bfee398c01f520c3275cd9dc159c6fbb28c5
Viden01 <edenvillabrille8@gmail.com>
2024-12-13 21:09:53 +0800

    commit

--- admin/private/attendance.php
+++ admin/private/attendance.php
@@ -48,9 +48,9 @@
                     <tbody>
                         <?php
                         // Fetch attendance records
-                        $sql = "SELECT *, employee_records.employee_id AS emp_id, employee_attendance.id AS attend 
+                        $sql = "SELECT *, employee_records.employee_id AS employee_id, employee_attendance.id AS attend 
                                 FROM employee_attendance 
-                                LEFT JOIN employee_records ON employee_records.emp_id = employee_attendance.employee_id 
+                                LEFT JOIN employee_records ON employee_records.employee_id = employee_attendance.employee_id 
                                 ORDER BY employee_attendance.date_attendance DESC, employee_attendance.time_in DESC";
                         $query = $conn->query($sql);
 
