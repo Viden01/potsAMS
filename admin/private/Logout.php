18575b3a0e717f7f42257e90ae9e9213679f23da
Viden01 <edenvillabrille8@gmail.com>
2024-12-14 02:59:24 +0800

    commit

--- admin/private/Logout.php
+++ admin/private/Logout.php
@@ -1,25 +1,26 @@
-
 <?php
 
 include '../connection/db_conn.php';
-// this is logout page when user click button logout in system page
+// This is the logout page when the user clicks the logout button in the system page.
 
 session_start();
-  date_default_timezone_set("Asia/Manila");
-  $time = date("M-d-Y h:i A",strtotime("+0 HOURS"));
+date_default_timezone_set("Asia/Manila");
+$time = date("M-d-Y h:i A", strtotime("+0 HOURS"));
 
- $email = $_SESSION['email_address'];
-  
+// Retrieve the user's email address from the session.
+$email = $_SESSION['email_address'];
 
-$conn->query("UPDATE history_log SET `logout_time` = '$time'  WHERE `id` = '$email'");
+// Update the logout time in the history_log table for the corresponding user ID.
+$conn->query("UPDATE history_log SET `logout_time` = '$time' WHERE `id` = '$email'");
 
+// Clear session data and destroy the session.
 $_SESSION = NULL;
 $_SESSION = [];
 session_unset();
 session_destroy();
 
+// Redirect to the index page with an alert.
 echo "<script type='text/javascript'>alert('LogOut Successfully!');
-				  document.location='../index.php'</script>";
+document.location='../index.php'</script>";
 
 ?>
-
