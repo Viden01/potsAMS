4b42aac3addc38105080eeb1d4905798dd0fc233
Viden01 <edenvillabrille8@gmail.com>
2024-12-24 03:14:26 +0800

    aa

--- admin/private/dashboard.php
+++ admin/private/dashboard.php
@@ -38,6 +38,7 @@ if (substr($request, -4) == '.php') {
   <link href="private/assets/css/main-style.css" rel="stylesheet" />
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
+  <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
   <style>
     /* Adjust donut chart size */
     #dashboardDonutChart {
@@ -179,8 +180,61 @@ if (substr($request, -4) == '.php') {
         <canvas id="dashboardDonutChart"></canvas> <!-- Donut Chart -->
       </div>
     </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            <div class="card-title">
+              Monthly Attendance Report
+            </div>
+          </div>
+          <div class="card-body">
+              <div id="chart"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+  <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+<script>
+   var options = {
+          series: [{
+            name: "Desktops",
+            data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        }],
+          chart: {
+          height: 350,
+          type: 'line',
+          zoom: {
+            enabled: false
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'straight'
+        },
+        title: {
+          text: 'Product Trends by Month',
+          align: 'left'
+        },
+        grid: {
+          row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            opacity: 0.5
+          },
+        },
+        xaxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+        }
+        };
 
+        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        chart.render();
+      
+      
+</script>
   <script>
   document.addEventListener('DOMContentLoaded', function() {
     // Bar Chart Configuration
