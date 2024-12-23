24288b0b64fae3708311a97dac0e215c2b64e15a
Viden01 <edenvillabrille8@gmail.com>
2024-12-24 02:14:00 +0800

    ass

--- admin/private/attendance.php
+++ admin/private/attendance.php
@@ -22,99 +22,97 @@
         <?php include 'modal/attendance_modal.php'; ?>
         <div class="panel-body">
             <div class="table-responsive">
-                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                    <thead>
-                        <th class="hidden"></th>
-                        <th>Employee ID</th>
-                        <th>Employee Name</th>
-                        <th>Time In</th>
-                        <th>Time Out</th>
-                        <th>Status</th>
-                        <th>Date</th>
-                        <th>Photo</th>
-                        <th>Location</th>
-                        <th>Action</th>
-                    </thead>
-                    <tbody>
-                    <?php
-$sql = "SELECT 
-employee_records.emp_id AS employee_id, 
-employee_records.first_name, 
-employee_records.last_name, 
-employee_attendance.time_in, 
-employee_attendance.time_out, 
-employee_attendance.status, 
-employee_attendance.date_attendance, 
-employee_attendance.photo_path,
-employee_attendance.latitude, 
-employee_attendance.longitude, 
-employee_attendance.id AS attend
-FROM employee_attendance 
-LEFT JOIN employee_records 
-ON employee_attendance.employee_id = employee_records.emp_id 
-ORDER BY employee_attendance.date_attendance DESC, employee_attendance.time_in DESC";
+            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+    <thead>
+        <th class="hidden"></th>
+        <th>Employee ID</th>
+        <th>Employee Name</th>
+        <th>Time In</th>
+        <th>Time Out</th>
+        <th>Status</th>
+        <th>Date</th>
+        <th>Photo</th>
+        <th>Location</th>
+        <th>Action</th>
+    </thead>
+    <tbody>
+    <?php
+    $sql = "SELECT 
+    employee_records.emp_id AS employee_id, 
+    employee_records.first_name, 
+    employee_records.last_name, 
+    employee_attendance.time_in, 
+    employee_attendance.time_out, 
+    employee_attendance.status, 
+    employee_attendance.date_attendance, 
+    employee_attendance.photo_path,
+    employee_attendance.latitude, 
+    employee_attendance.longitude, 
+    employee_attendance.id AS attend
+    FROM employee_attendance 
+    LEFT JOIN employee_records 
+    ON employee_attendance.employee_id = employee_records.emp_id 
+    ORDER BY employee_attendance.date_attendance DESC, employee_attendance.time_in DESC";
 
-$query = $conn->query($sql);
+    $query = $conn->query($sql);
 
-if ($query === FALSE) {
-echo "<tr><td colspan='9'>Error fetching records: " . $conn->error . "</td></tr>";
-} elseif ($query->num_rows == 0) {
-echo "<tr><td colspan='9'>No attendance records found.</td></tr>";
-} else {
-while ($row = $query->fetch_assoc()) {
-if (empty($row['first_name']) || empty($row['last_name'])) {
-error_log("Unmatched employee_id in attendance: " . htmlentities($row['employee_id']));
-continue;
-}
+    if ($query === FALSE) {
+        echo "<tr><td colspan='9'>Error fetching records: " . $conn->error . "</td></tr>";
+    } elseif ($query->num_rows == 0) {
+        echo "<tr><td colspan='9'>No attendance records found.</td></tr>";
+    } else {
+        while ($row = $query->fetch_assoc()) {
+            if (empty($row['first_name']) || empty($row['last_name'])) {
+                error_log("Unmatched employee_id in attendance: " . htmlentities($row['employee_id']));
+                continue;
+            }
 
-$time_in_display = !empty($row['time_in']) ? $row['time_in'] : 'n/a';
-$time_out_display = !empty($row['time_out']) ? $row['time_out'] : 'n/a';
+            $time_in_display = !empty($row['time_in']) ? $row['time_in'] : 'n/a';
+            $time_out_display = !empty($row['time_out']) ? $row['time_out'] : 'n/a';
 
-$status = ($row['status']) 
-? '<button class="btn btn-success btn-xs"><i class="fa fa-check"></i> On Time</button>' 
-: '<button class="btn btn-danger btn-xs"><i class="fa fa-times"></i> Late</button>';
+            $status = ($row['status']) 
+            ? '<button class="btn btn-success btn-xs"><i class="fa fa-check"></i> On Time</button>' 
+            : '<button class="btn btn-danger btn-xs"><i class="fa fa-times"></i> Late</button>';
 
-// Display photo if available
-$photo_display = !empty($row['photo_path']) 
-? "<img src='uploads/" . htmlentities($row['photo_path']) . "' style='width: 50px; height: 50px;' alt='Photo'>" 
-: "No Photo";
+            // Display photo if available
+            $photo_display = !empty($row['photo_path']) 
+            ? "<img src='uploads/" . htmlentities($row['photo_path']) . "' style='width: 50px; height: 50px;' alt='Photo'>" 
+            : "No Photo";
 
-// Generate the Google Maps link for the location
-$location_display = '';
-if (!empty($row['latitude']) && !empty($row['longitude'])) {
-$latitude = htmlentities($row['latitude']);
-$longitude = htmlentities($row['longitude']);
-$location_display = '<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d62627.84334478038!2d123.7901672!3d11.1698423!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sph!4v1734977097856!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
-} else {
-$location_display = "No Location Available";
-}
+            // Generate the Google Maps link for the location
+            $location_display = '';
+            if (!empty($row['latitude']) && !empty($row['longitude'])) {
+                $latitude = htmlentities($row['latitude']);
+                $longitude = htmlentities($row['longitude']);
+                $location_display = '<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d62627.84334478038!2d' . $longitude . '!3d' . $latitude . '!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sph!4v1734977097856!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+            } else {
+                $location_display = "No Location Available";
+            }
 
-echo "
-<tr>
-    <td class='hidden'></td>
-    <td>".htmlentities($row['employee_id'])."</td>
-    <td>".htmlentities($row['first_name'].' '.$row['last_name'])."</td>
-    <td>".htmlentities($time_in_display)."</td>
-    <td>".htmlentities($time_out_display)."</td>
-    <td>".$status."</td>
-    <td>".date('M d, Y', strtotime(htmlentities($row['date_attendance'])))."</td>
-    <td>".$photo_display."</td>
-    <td>".$location_display."</td>
-    <td>
-        <button class='btn btn-danger btn-sm btn-flat delete' data-id='".htmlentities($row['attend'])."'>
-            <i class='fa fa-trash'></i> Delete
-        </button>
-    </td>
-</tr>
-";
-}
-}
+            echo "
+            <tr>
+                <td class='hidden'></td>
+                <td>".htmlentities($row['employee_id'])."</td>
+                <td>".htmlentities($row['first_name'].' '.$row['last_name'])."</td>
+                <td>".htmlentities($time_in_display)."</td>
+                <td>".htmlentities($time_out_display)."</td>
+                <td>".$status."</td>
+                <td>".date('M d, Y', strtotime(htmlentities($row['date_attendance'])))."</td>
+                <td>".$photo_display."</td>
+                <td>".$location_display."</td>
+                <td>
+                    <button class='btn btn-danger btn-sm btn-flat delete' data-id='".htmlentities($row['attend'])."'>
+                        <i class='fa fa-trash'></i> Delete
+                    </button>
+                </td>
+            </tr>
+            ";
+        }
+    }
+    ?>
+    </tbody>
+</table>
 
-?>
-
-
-                    </tbody>
-                </table>
             </div>
         </div>
     </div>
