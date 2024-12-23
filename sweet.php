f2948c1ebcc5697e4f0151226ee5cb4dc4998f95
louilal123 <146294063+louilal123@users.noreply.github.com>
2024-12-23 22:41:08 +0800

    Create sweet.php

---
+++ sweet.php
@@ -0,0 +1,16 @@
+<!-- crud sweetalerts  this is included inside all the pages below uaing include-->
+<?php
+if (isset($_SESSION['status']) && $_SESSION['status'] != '') {
+?>
+<script>
+Swal.fire({
+    icon: "<?php echo $_SESSION['status_icon']; ?>",
+    title: "<?php echo $_SESSION['status']; ?>",
+    confirmButtonText: "Ok"
+});
+</script>
+<?php
+unset($_SESSION['status']);
+unset($_SESSION['status_icon']);
+}
+?>
