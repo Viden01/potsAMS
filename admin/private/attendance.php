d565c7171cc5a19c3fd4c86c7a9adca5bddf529c
Viden01 <edenvillabrille8@gmail.com>
2024-12-24 00:57:59 +0800

    aaa

--- admin/private/attendance.php
+++ admin/private/attendance.php
@@ -68,10 +68,11 @@ if ($query === FALSE) {
             continue; // Skip rows with no matching employee record
         }
 
-        // Use 24-hour time format (H:i:s)
-        $time_in_display = !empty($row['time_in']) ? date('H:i:s', strtotime(htmlentities($row['time_in']))) : '00:00:00';
-        $time_out_display = !empty($row['time_out']) ? date('H:i:s', strtotime(htmlentities($row['time_out']))) : '00:00:00';
-
+         // Use 12-hour time format (h:i A)
+         $time_in_display = !empty($row['time_in']) ? date('h:i A', strtotime(htmlentities($row['time_in']))) : '12:00 AM';
+         $time_out_display = !empty($row['time_out']) ? date('h:i A', strtotime(htmlentities($row['time_out']))) : '12:00 AM';
+ 
+      
         $status = ($row['status']) 
             ? '<button class="btn btn-success btn-xs"><i class="fa fa-check"></i> On Time</button>' 
             : '<button class="btn btn-danger btn-xs"><i class="fa fa-times"></i> Late</button>';
